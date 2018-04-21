f586ffcbc73a64bba130d16f29ac95ca6de35125
Julian Tutuncu-Macias <jtutuncumacias@gmail.com>
2018-04-21 15:46:33 -0400

    replaced "displayUser" function

--- js/app.js
+++ js/app.js
@@ -6,26 +6,21 @@ var api_sig = '&api_sig='+md5("api_keycf750ccfe46454e1247540a136b2f2f4methodauth
 var session = url+'?method=auth.getsession&'+token.toString()+api_key+api_sig+'&format=json'
 
 
-function displayUser() {
-	console.log(this.responseText);
-	console.log(JSON.parse(this.responseText));
-
-	var response = JSON.parse(xhttp.responseText);
-	console.log(response);
-	
-	var name = response.session.name
-	var key = response.session.key
-	console.log(name);
-	console.log(key);
-
-	document.getElementById("info").textContent = name;
-}
-
-
 var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
-      		displayUser();
+      		console.log(this.responseText);
+			console.log(JSON.parse(this.responseText));
+
+			var response = JSON.parse(xhttp.responseText);
+			console.log(response);
+	
+			var name = response.session.name
+			var key = response.session.key
+			console.log(name);
+			console.log(key);
+
+			document.getElementById("info").textContent = name;
     	}
   	};
   	xhttp.open("GET", session, true);
