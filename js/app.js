648927f171329b0f044fc6d87a7c91b3caff1d93
Julian Tutuncu-Macias <jtutuncumacias@gmail.com>
2018-04-21 15:12:08 -0400

    switch to async
    pre-emptive tests against XmlHttpRequest response issues with storing objects

--- js/app.js
+++ js/app.js
@@ -1,26 +1,25 @@
 var url = 'https://ws.audioscrobbler.com/2.0/'
 
-var tokenParam = new URLSearchParams(window.location.search);
-var token = tokenParam.toString();
-console.log(token);
-console.log(tokenParam.get("token"));
-
-var key = '&api_key=cf750ccfe46454e1247540a136b2f2f4'
-var sig = '&api_sig='+md5("api_keycf750ccfe46454e1247540a136b2f2f4methodauth.getsessiontoken"+tokenParam.get("token")+"c486fea288f58b9777900fee760af2e1")
-
-var session = url+'?method=auth.getsession&'+token+key+sig+'&format=json'
-
+var token = new URLSearchParams(window.location.search);
+var api_key = '&api_key=cf750ccfe46454e1247540a136b2f2f4'
+var api_sig = '&api_sig='+md5("api_keycf750ccfe46454e1247540a136b2f2f4methodauth.getsessiontoken"+token.get("token")+"c486fea288f58b9777900fee760af2e1")
+var session = url+'?method=auth.getsession&'+token.toString()+api_key+api_sig+'&format=json'
 
 var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", session, false);
+    xhttp.open("GET", session, true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
-    var response = JSON.parse(xhttp.responseText);
+
+console.log(xhttp.responseText);
+console.log(JSON.parse(xhttp.responseText));
+
+var response = JSON.parse(xhttp.responseText);
 console.log(response);
 
-var session_key = response.session.key
 var name = response.session.name
+var key = response.session.key
 
 console.log(name);
+console.log(key);
 
 document.getElementById("info").textContent = name;
