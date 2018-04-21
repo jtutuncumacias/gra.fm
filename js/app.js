9c3640285961e56ea798d8eba01a56fe43df2188
Julian Tutuncu-Macias <jtutuncumacias@gmail.com>
2018-04-21 15:38:01 -0400

    refactor code into callback function

--- js/app.js
+++ js/app.js
@@ -5,21 +5,30 @@ var api_key = '&api_key=cf750ccfe46454e1247540a136b2f2f4'
 var api_sig = '&api_sig='+md5("api_keycf750ccfe46454e1247540a136b2f2f4methodauth.getsessiontoken"+token.get("token")+"c486fea288f58b9777900fee760af2e1")
 var session = url+'?method=auth.getsession&'+token.toString()+api_key+api_sig+'&format=json'
 
+
+function displayUser() {
+	console.log(this.responseText);
+	console.log(JSON.parse(this.responseText));
+
+	var response = JSON.parse(xhttp.responseText);
+	console.log(response);
+	
+	var name = response.session.name
+	var key = response.session.key
+	console.log(name);
+	console.log(key);
+
+	document.getElementById("info").textContent = name;
+}
+
+
 var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", session, true);
+    xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+      		displayUser();
+    	}
+  	};
+  	xhttp.open("GET", session, true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
 
-console.log(xhttp.responseText);
-console.log(JSON.parse(xhttp.responseText));
-
-var response = JSON.parse(xhttp.responseText);
-console.log(response);
-
-var name = response.session.name
-var key = response.session.key
-
-console.log(name);
-console.log(key);
-
-document.getElementById("info").textContent = name;
