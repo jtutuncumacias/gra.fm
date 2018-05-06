cab492a1369e3de4886ca076f1d4d690587be8e2
Julian Tutuncu-Macias <jtutuncumacias@gmail.com>
2018-05-05 23:37:51 -0400

    repair friendList functionality

--- js/app.js
+++ js/app.js
@@ -6,40 +6,44 @@ var api_sig = '&api_sig='+md5("api_keycf750ccfe46454e1247540a136b2f2f4methodauth
 var session = url+'?method=auth.getsession&'+token.toString()+api_key+api_sig+'&format=json'
 
 //login
-var user;
-var key;
+//localStorage.setItem('username', "buypil");
 
 var login = new XMLHttpRequest();
-login.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		var response = JSON.parse(login.responseText);
-		user = response.session.name;
-		key = response.session.key;
-
-		document.getElementById("info").textContent = user;
-	}
-};
 login.open("GET", session, true);
 login.setRequestHeader("Content-type", "application/json");
 login.send();
-
-//get friends
-var friend_url = "?method=user.getfriends&user=" + user + api_key + "&format=json"
-var friendList = [];
-
-var friends = new XMLHttpRequest();
-friends.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-  		var response = JSON.parse(friends.responseText);
+login.onreadystatechange = function() {
+	if (login.readyState == 4 && login.status == 200) {
+		var response = JSON.parse(login.responseText);
 		console.log(response);
 
-		for (friend in response.friends) {
-			console.log("pushing a new friend");
-			console.log(friend);
-			friendList.push(friend);
-		}
+		localStorage.setItem('username', response.session.name);
+		localStorage.setItem('key', response.session.key);
+
+		document.getElementById("info").textContent = localStorage.username;
 	}
 };
+
+//get friends
+var friend_url = url + "?method=user.getfriends&user=" + localStorage.getItem("username") + api_key + "&format=json"
+
+var friends = new XMLHttpRequest();
 friends.open("GET", friend_url, true);
 friends.setRequestHeader("Content-type", "application/json");
 friends.send();
+friends.onreadystatechange = function() {
+	if (friends.readyState == 4 && friends.status == 200) {
+  		var response = JSON.parse(friends.responseText);
+		
+		var friendList = [];
+
+		for (var i = 0; i < response.friends["@attr"].total; i++) {
+			var friend = response.friends.user[i];
+			//console.log(friend);
+			friendList.push(friend);
+		}
+		localStorage.setItem('friendList', friendList);
+	}
+};
+
+console.log(localStorage.getItem("friendList"));
